665581bfb93003de03494db1cac5a55fbbfd7319
IamTheeIrianna <irpo0001@stud.kea.dk>
2025-03-27 14:06:05 +0100

    email added

--- src/main/java/model/UserEntity.java
+++ src/main/java/model/UserEntity.java
@@ -4,12 +4,14 @@ import java.util.List;
 public class UserEntity {
     private String username;
     private String password;
+    private String email;
     private int userId;
     private List<WishListItem> wishListItem;
 
-    public UserEntity(String username, String password, int userId, List<WishListItem> wishListItem){
+    public UserEntity(String username, String password, String email, int userId, List<WishListItem> wishListItem){
         this.username = username;
         this.password = password;
+        this.email = email;
         this.userId = userId;
         this.wishListItem = wishListItem;
     }
@@ -21,6 +23,9 @@ public class UserEntity {
     }
     public void setPassword(String password){
         this.password = password;
+    }
+    public void setEmail(String email){
+        this.email = email;
     }
     public void setUserId(int userId){
         this.userId = userId;
@@ -35,6 +40,9 @@ public class UserEntity {
     }
     public String getPassword(){
         return password;
+    }
+    public String getEmail(){
+        return email;
     }
     private int getUserId(){
         return userId;
