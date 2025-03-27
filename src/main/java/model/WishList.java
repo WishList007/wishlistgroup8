1651713f936f965de7a6b0e13964f6a8e841d6f5
IamTheeIrianna <irpo0001@stud.kea.dk>
2025-03-27 14:41:18 +0100

    first commit, wishList & wishListItems

--- src/main/java/model/WishList.java
+++ src/main/java/model/WishList.java
@@ -1,4 +1,47 @@
 package model;
 
+import java.util.List;
+
 public class WishList {
+    private int wishListId;
+    private String wishListName;
+    private String wishListDescription;
+    private List<WishListItem> wishListItems;
+
+    public WishList(int wishListId, String wishListName,String wishListDescription,List<WishListItem>wishListItems){
+        this.wishListId = wishListId;
+        this.wishListName = wishListName;
+        this.wishListDescription = wishListDescription;
+        this.wishListItems = wishListItems;
+    }
+    public WishList(){
+    }
+    //----------setters----------
+    public void setWishListId(int wishListId){
+        this.wishListId = wishListId;
+    }
+    public void setWishListName(String wishListName){
+        this.wishListName = wishListName;
+    }
+    public void setWishListDescription(String wishListDescription){
+        this.wishListDescription = wishListDescription;
+    }
+    public void setWishListItems(List<WishListItem>wishListItems){
+        this.wishListItems = wishListItems;
+    }
+    //----------Getters-----------
+    public int getWishListId(){
+        return wishListId;
+    }
+    public String getWishListName(){
+        return wishListName;
+    }
+    public String getWishListDescription(){
+        return wishListDescription;
+    }
+    public List<WishListItem> getWishListItems(){
+        return wishListItems;
+
+    }
+
 }
--- src/main/java/model/WishListItem.java
+++ src/main/java/model/WishListItem.java
@@ -1,4 +1,53 @@
 package model;
 
 public class WishListItem {
+private int itemId;
+private String itemName;
+private String itemDescription;
+private double itemPrice;
+private String itemLink;
+
+public WishListItem(int itemId,String itemName,String itemDescription,double itemPrice,String itemLink){
+    this.itemId = itemId;
+    this.itemName = itemName;
+    this.itemDescription = itemDescription;
+    this.itemPrice = itemPrice;
+    this.itemLink=itemLink;
+}
+public WishListItem(){
+
+}
+//--------Getters----------
+    public int getItemId(){
+    return itemId;
+    }
+    public String getItemName(){
+    return itemName;
+    }
+    public String getItemDescription(){
+    return itemDescription;
+    }
+    public double getItemPrice(){
+    return itemPrice;
+    }
+    public String getItemLink(){
+    return itemLink;
+    }
+    //-----------------Setters----------
+    public void setItemId(int itemId){
+    this.itemId = itemId;
+    }
+    public void setItemName (String itemName){
+    this.itemName = itemName;
+    }
+    public void setItemDescription (String itemDescription){
+    this.itemDescription = itemDescription;
+    }
+    public void setItemPrice (double itemPrice){
+this.itemPrice = itemPrice;
+    }
+    public void getItemLink(String itemLink){
+    this.itemLink = itemLink;
+    }
+
 }
