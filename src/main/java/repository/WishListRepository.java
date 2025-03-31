d69f18509cb175601a5efa3a2a5a4d21ea5ffc87
christianvinther <christian@vinther.dk>
2025-03-31 13:17:37 +0200

    Test

--- src/main/java/repository/WishListRepository.java
+++ src/main/java/repository/WishListRepository.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+// hey
 
 import java.util.List;
 
