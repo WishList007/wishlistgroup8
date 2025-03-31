73a95c084082ec3fb49f0dcaed58a04b1e861870
IamTheeIrianna <irpo0001@stud.kea.dk>
2025-03-31 11:42:36 +0200

    admin controller class update & Exception handling error message class

--- src/main/java/Controller/AdminController.java
+++ src/main/java/Controller/AdminController.java
@@ -10,10 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import service.AdminService;
+
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
 
     @Autowired
     private AdminService adminService;
+
+    @PostMapping("/logIn")
+    public ResponseEntity<String>userHandlerLogIn(Authentication auth)throws ErrorMessage
 }
