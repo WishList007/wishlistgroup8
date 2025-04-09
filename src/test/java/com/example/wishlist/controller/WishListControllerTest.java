370ae59e3b5934fc07f77c36455a01fbc6310b39
christianvinther <christian@vinther.dk>
2025-04-09 12:04:13 +0200

    edits to wishlistcontroller

--- src/test/java/com/example/wishlist/controller/WishListControllerTest.java
+++ src/test/java/com/example/wishlist/controller/WishListControllerTest.java
@@ -1,27 +1,30 @@
 package com.example.wishlist.controller;
 
-import com.example.wishlist.model.UserEntity;
-import com.example.wishlist.model.WishList;
-import com.example.wishlist.model.WishListItem;
-import com.example.wishlist.service.WishListService;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import com.example.wishlist.model.UserEntity;
+import com.example.wishlist.model.WishList;
+import com.example.wishlist.model.WishListItem;
+import com.example.wishlist.service.WishListService;
 
 @WebMvcTest(WishListController.class)
 class WishListControllerTest {
